7f59fc3a73011ff388850942a7e9596a742fc0ae
gommosvn <gommosvn@97971f44-ecf5-0310-a927-8eb58852784f>
2005-04-30 00:27:26 +0000

    Moved some files

--- trunk/MaddenEditor/Db/TdbFieldProperties.cs
+++ trunk/MaddenEditor/Db/TdbFieldProperties.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MaddenEditor.Domain
+namespace MaddenEditor.Db
 {
     enum TdbFieldType { tdbString = 0, tdbBinary = 1, tdbSInt = 2, tdbUInt = 3, tdbFloat = 4, tdbInt = 5 };
 
