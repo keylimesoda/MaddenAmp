f3b02ccad0a6be1ce7a1cffe221be23a5466ea78
gommosvn <gommosvn@97971f44-ecf5-0310-a927-8eb58852784f>
2005-05-16 10:44:06 +0000

    #18 Fixed up user controlled teams. Doesnt seem to take effect until season end

--- MaddenEditor/Core/Record/OwnerRecord.cs
+++ MaddenEditor/Core/Record/OwnerRecord.cs
@@ -8,6 +8,13 @@ namespace MaddenEditor.Core.Record
 	{
 		public const string TEAM_ID = "TGID";
 		public const string USER_CONTROLLED = "CFUC";
+		public const string COMPUTER_CONTROL_1 = "CFDA";
+		public const string COMPUTER_CONTROL_2 = "CFFA";
+		public const string COMPUTER_CONTROL_3 = "CFDP";
+		public const string COMPUTER_CONTROL_4 = "CFRP";
+		public const string COMPUTER_CONTROL_5 = "CFFR";
+		public const string COMPUTER_CONTROL_6 = "CFRR";
+		public const string COMPUTER_CONTROL_7 = "CFEX";
 
 		public OwnerRecord(int record, EditorModel EditorModel)
 			: base(record, EditorModel)
@@ -49,6 +56,90 @@ namespace MaddenEditor.Core.Record
 			set
 			{
 				SetField(USER_CONTROLLED, Convert.ToInt32(value));
+			}
+		}
+
+		public bool ComputerControl1
+		{
+			get
+			{
+				return (GetIntField(COMPUTER_CONTROL_1) == 1);
+			}
+			set
+			{
+				SetField(COMPUTER_CONTROL_1, Convert.ToInt32(value));
+			}
+		}
+
+		public bool ComputerControl2
+		{
+			get
+			{
+				return (GetIntField(COMPUTER_CONTROL_2) == 1);
+			}
+			set
+			{
+				SetField(COMPUTER_CONTROL_2, Convert.ToInt32(value));
+			}
+		}
+
+		public bool ComputerControl3
+		{
+			get
+			{
+				return (GetIntField(COMPUTER_CONTROL_3) == 1);
+			}
+			set
+			{
+				SetField(COMPUTER_CONTROL_3, Convert.ToInt32(value));
+			}
+		}
+
+		public bool ComputerControl4
+		{
+			get
+			{
+				return (GetIntField(COMPUTER_CONTROL_4) == 1);
+			}
+			set
+			{
+				SetField(COMPUTER_CONTROL_4, Convert.ToInt32(value));
+			}
+		}
+
+		public bool ComputerControl5
+		{
+			get
+			{
+				return (GetIntField(COMPUTER_CONTROL_5) == 1);
+			}
+			set
+			{
+				SetField(COMPUTER_CONTROL_5, Convert.ToInt32(value));
+			}
+		}
+
+		public bool ComputerControl6
+		{
+			get
+			{
+				return (GetIntField(COMPUTER_CONTROL_6) == 1);
+			}
+			set
+			{
+				SetField(COMPUTER_CONTROL_6, Convert.ToInt32(value));
+			}
+		}
+
+		public bool ComputerControl7
+		{
+			get
+			{
+				return (GetIntField(COMPUTER_CONTROL_7) == 1);
+			}
+			set
+			{
+				SetField(COMPUTER_CONTROL_7, Convert.ToInt32(value));
 			}
 		}
 	}
