8ea6c205f4d62a4f8d1d6e3d10afdfe06c176e7a
spin16svn <spin16svn@97971f44-ecf5-0310-a927-8eb58852784f>
2005-12-13 00:13:04 +0000

    Another minor change, same issue.

--- MaddenEditor/Forms/WeeklyMaintenanceForm.cs
+++ MaddenEditor/Forms/WeeklyMaintenanceForm.cs
@@ -69,6 +69,7 @@ namespace MaddenEditor.Forms
             }
 
             sr.Close();
+            File.Delete(installDirectory + "\\ratings\\" + profile);
         }
 
         private void GetProfile()
